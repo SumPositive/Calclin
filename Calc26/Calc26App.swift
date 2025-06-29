c1d76ea634de3af29f0f8bd1f8054b1ad6434a18
SumPositive <sp@art.jp>
2025-06-29 12:46:17 +0900

    Initial Commit

---
+++ Calc26/Calc26App.swift
@@ -0,0 +1,17 @@
+//
+//  Calc26App.swift
+//  Calc26
+//
+//  Created by Sum Positive on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct Calc26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calc26Tests/Calc26Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Calc26Tests.swift
+//  Calc26Tests
+//
+//  Created by Sum Positive on 2025/06/29.
+//
+
+import Testing
+@testable import Calc26
+
+struct Calc26Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
